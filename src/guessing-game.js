fa7f25da01bcadae220bdfcfc1e2ad6edca0ce7f
zhulym <zhulym@mail.ru>
2021-08-16 14:26:28 +0300

    feat: first solution 0/100

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() { }
+    constructor() {
+        this.min = '';
+        this.max = '';
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return Math.floor((max - min) / 2);
     }
 
     lower() {
+        this.min = min;
+        this.max = result;
 
     }
 
     greater() {
-
+        this.min = result;
+        this.max = max;
     }
 }
 
